d3e7f3f628e28d47cbe5f6abb4419f2cbd746d5a
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-31 00:27:25 +0530

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,4 +1,16 @@
-cd $SRC/.clusterfuzzlite
-chmod +x ./build_afl.bash
+set -e
+CC=${CC:-clang}
+CXX=${CXX:-clang++}
 
-$CC -fsanitize=address -fsanitize-coverage=trace-pc-guard fuzzer.cc FuzzingEngine.a -o fuzzer
+# Make sure we don't clobber anything in the current directory.
+rm -rf afl-build
+git clone --depth=1 https://github.com/AFLplusplus/AFLplusplus afl-build
+cd afl-build
+make source-only
+ar ru FuzzingEngine.a afl-compiler-rt.o utils/aflpp_driver/aflpp_driver.o
+
+cp -f FuzzingEngine.a afl-fuzz afl-showmap ../
+ls -lrth
+echo "Success: link fuzz target against FuzzingEngine.a!"
+
+$CC -fsanitize=address -fsanitize-coverage=trace-pc-guard imageRead.c FuzzingEngine.a -o imageRead
